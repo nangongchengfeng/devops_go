208cc98543992d4adeb9a7526561a9af2bb3e86a
南宫乘风 <1794748404@qq.com>
2023-10-16 11:17:13 +0800

    【增加】 Gin初始化

--- mian.go
+++ mian.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"devops_go/config"
+	"devops_go/controller"
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	r := gin.Default()
+	controller.Router.InitApiRouter(r)
+	r.Run(config.ListenAddr)
 }
